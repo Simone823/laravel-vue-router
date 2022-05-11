ab56a0dd6694675fc3831d87c8e6c7c824ffc32f
Simone823 <simone.daglio823@outlook.it>
2022-05-11 16:22:51 +0200

    metodo archive api category controller

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Category;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // Recupero le categorie dal db
+        $categories = Category::all();
+
+        // Return response json categories
+        return response()->json([
+            'categories' => $categories,
+            'success' => true
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    // Funzione archive category
+    public function archive($slug) {
+
+        // Recupero la categoria in base allo slug
+        $category = Category::with('posts')->where('slug', '=', $slug)->first();
+
+        // Return response json categories posts
+        return response()->json([
+            'category' => $category,
+            'posts' => $category->posts,
+            'success' => true
+        ]);
+    }
+}
