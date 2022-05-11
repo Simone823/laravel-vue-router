d25b9a82bce5b47b4acd2e8b4dea4867883eb728
Simone823 <simone.daglio823@outlook.it>
2022-05-11 13:24:09 +0200

    import layout default homepage vue

--- resources/js/pages/Homepage.vue
+++ resources/js/pages/Homepage.vue
@@ -1,12 +1,24 @@
 <template>
-    <div>
-        <h1>Homepage</h1>
-    </div>
+    
+    <!-- extend layout.vue -->
+    <LayoutDefault>
+        <div>
+            <h1>Homepage</h1>
+        </div>
+    </LayoutDefault>
+
 </template>
 
 <script>
+// Import layout default
+import LayoutDefault from '../layouts/layout.vue';
+
     export default {
         name: 'Homepage',
+
+        components: {
+            LayoutDefault,
+        },
     }
 </script>
 
