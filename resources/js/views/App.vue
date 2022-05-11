1533f98e264da60071fb9efb4269c86f724bcdd2
Simone823 <simone.daglio823@outlook.it>
2022-05-11 13:13:26 +0200

    modifica app, router-view

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,30 +1,7 @@
 <template>
 
-    <!-- App vue -->
-    <div id="app" class="bg-stone-800 text-white">
-
-        <!-- Header nav -->
-        <header class="py-8 px-4 mb-4">
-            <nav class="nav-menu">
-                <ul class="flex items-center justify-center gap-8 uppercase">
-                    <li>
-                        <router-link to="/">Homepage</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/posts">Posts</router-link>
-                    </li>
-                    <li>
-                        <router-link to="">Categories</router-link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-
-        <!-- Main -->
-        <main>
-            <router-view></router-view>
-        </main>
-    </div>
+    <!-- Roouter view -->
+    <router-view></router-view>
     
 </template>
 
