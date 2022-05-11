1f7ac918a2aec34a857a45cd91f63e06f460332c
Simone823 <simone.daglio823@outlook.it>
2022-05-11 16:05:13 +0200

    import layout posts index

--- resources/js/pages/Posts.index.vue
+++ resources/js/pages/Posts.index.vue
@@ -1,40 +1,47 @@
 <template>
 
-    <!-- Section posts index -->
-    <section class="posts_index container py-8 px-4">
-
-        <!-- Grid container posts cards wrapper -->
-        <div class="posts_cards_wrapper grid grid-cols-1 md:grid-cols-3 lg:grid:cols-4 sm:grid-cols-2 gap-16 mb-16">
-            <!-- Componente card post -->
-            <PostCard v-for="element in postsArray" :key="element.id" :arrayPosts="element"/>
-        </div>
-
-        <!-- Pages -->
-        <ul class="pages_control flex items-center justify-center gap-8 flex-wrap">
-           <li v-for="number in lastPage" :key="number" 
-                :class="[
-                    currentPage === number ? 'bg-gradient-to-r from-pink-500 to-yellow-500' : 'bg-gradient-to-r from-cyan-500 to-blue-500', 
-                    'dot hover:from-pink-500 hover:to-yellow-500 rounded-full h-11 w-11 flex items-center justify-center cursor-pointer text-base font-bold'
-                ]"
-                @click="recuperoPosts(number)"
-            >
-                {{number}}
-            </li> 
-        </ul>
-
-    </section>
+    <!-- extend layout.vue -->
+    <LayoutDefault>
+        <!-- Section posts index -->
+        <section class="posts_index container py-8 px-4">
+    
+            <!-- Grid container posts cards wrapper -->
+            <div class="posts_cards_wrapper grid grid-cols-1 md:grid-cols-3 lg:grid:cols-4 sm:grid-cols-2 gap-16 mb-16">
+                <!-- Componente card post -->
+                <PostCard v-for="element in postsArray" :key="element.id" :arrayPosts="element"/>
+            </div>
+    
+            <!-- Pages -->
+            <ul class="pages_control flex items-center justify-center gap-8 flex-wrap">
+               <li v-for="number in lastPage" :key="number" 
+                    :class="[
+                        currentPage === number ? 'bg-gradient-to-r from-pink-500 to-yellow-500' : 'bg-gradient-to-r from-cyan-500 to-blue-500', 
+                        'dot hover:from-pink-500 hover:to-yellow-500 rounded-full h-11 w-11 flex items-center justify-center cursor-pointer text-base font-bold'
+                    ]"
+                    @click="recuperoPosts(number)"
+                >
+                    {{number}}
+                </li> 
+            </ul>
+    
+        </section>
+    </LayoutDefault>
 
 </template>
 
 <script>
 import Axios from "axios";
 
+// Import layout
+import LayoutDefault from '../layouts/layout.vue';
+
 // Import post card
 import PostCard from '../components/CardPost.vue';
 
 export default {
     components: {
-        PostCard
+        LayoutDefault,
+        PostCard,
     },
 
     data() {
@@ -58,7 +65,7 @@ export default {
         recuperoPosts: function(page = 1) {
 
             // Axios
-            Axios.get('http://127.0.0.1:8000/api/posts', {
+            Axios.get('/api/posts', {
                 params: {
                     page: page, 
                 }
@@ -84,7 +91,7 @@ export default {
 
             // Scroll to top 0px
             window.scrollTo({ top: 0, behavior: 'smooth' });
-        }
+        },
     },
 
     mounted() {
