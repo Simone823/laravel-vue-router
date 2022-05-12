a7a4654257a8127b4ca257245b795644e7da6d36
Simone823 <simone.daglio823@outlook.it>
2022-05-12 18:42:33 +0200

    modifica colori posts.show

--- resources/js/pages/Posts.show.vue
+++ resources/js/pages/Posts.show.vue
@@ -6,18 +6,19 @@
         <div v-if="post" class="card_post p-4">
 
             <!-- Img wrapper -->
-            <figure class="img_wrapper_post w-full mb-3 shadow-2xl">
+            <figure class="img_wrapper_post w-full mb-3 shadow-lg shadow-white/30">
                 <img :src="post.image" alt="" class="w-full object-cover object-center">
             </figure>
 
             <!-- Description card -->
             <div class="description_post py-8 px-5 container">
-                <h3 class="title mb-5 text-2xl">{{post.title}}</h3>
-                <p v-if="post.category" class="category mb-5 text-yellow-500 text-base font-semibold">{{post.category.name}}</p>
+                <h3 class="title mb-8 text-3xl max-w-max">{{post.title}}</h3>
+                <p v-if="post.category" class="category rounded rounded-md bg-color-lime-1 shadow-md shadow-color-lime-1/50 text-black font-semibold max-w-max py-1 px-4 mb-8">{{post.category.name}}</p>
 
                 <!-- list Tags -->
                 <ul v-if="post.tags" class="list_tags flex gap-5 items-center flex-wrap mb-8">
-                    <li class="tag rounded-full bg-gradient-to-r from-blue-500 to-indigo-500 py-1 px-4 font-semibold" v-for="tag in post.tags" :key="tag.id">
+                    <li class="tag rounded rounded-lg bg-color-blue-1 text-black shadow-md shadow-color-blue-1/50 py-1 px-4 font-semibold flex items-center gap-1" v-for="tag in post.tags" :key="tag.id">
+                        <img :src="require('/public/img/tag_icon.svg')" class="h-4 w-4" alt="">
                         {{tag.name}}
                     </li>
                 </ul>
