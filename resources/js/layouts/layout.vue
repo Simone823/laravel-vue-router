c8661f582e1525b0897dbdc52b1347739509387b
Simone823 <simone.daglio823@outlook.it>
2022-05-14 18:11:39 +0200

    aggiunta logo header layout

--- resources/js/layouts/layout.vue
+++ resources/js/layouts/layout.vue
@@ -4,9 +4,18 @@
     <div id="app" class="layout_default bg-stone-800 text-white">
 
         <!-- Header nav -->
-        <header class="py-8 px-4 mb-4 bg-stone-900 shadow-md shadow-white/30 w-full fixed z-50">
-            <nav class="nav-menu">
-                <ul class="flex items-center justify-center gap-8 uppercase tracking-widest font-semibold text-color-blue-3">
+        <header class="py-4 px-8 bg-stone-900 shadow-sm shadow-white/30 w-full fixed z-50 flex items-center gap-x-48">
+
+            <!-- Logo -->
+            <figure class="logo_wrapper h-16 w-72">
+                <router-link to="/">
+                    <img :src="require('/public/img/boolpress_logo.svg')" alt="">
+                </router-link>
+            </figure>
+
+            <!-- Nav link -->
+            <nav class="nav-menu grow">
+                <ul class="flex items-center gap-8 uppercase tracking-widest font-semibold text-color-blue-3">
                     <li>
                         <router-link class="hover:text-white duration-500" to="/">Homepage</router-link>
                     </li>
